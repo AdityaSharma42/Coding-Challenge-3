5aebb3e24e9e1db2c05fba61d72e73ab63cef021
AdityaSharma.42 <adityasharma3@usf.edu>
2024-09-08 18:10:17 -0400

    Implement calculateAverageSales function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,3 +1,6 @@
 function calculateAverageSales(Sales) {
 return Sales.length===0? 0: Sales.reduce((sum,Sale)=> sum+ Sale,0)/Sales.length;
 }
+let SalesFigures= [4590,18400,2130,8800]
+let average = calculateAverageSales (SalesFigures);
+console.log ('The average sales are', average);
