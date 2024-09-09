fd38093e0af080372cbe6657a8a2b26c59feef8b
AdityaSharma.42 <adityasharma3@usf.edu>
2024-09-08 23:52:41 -0400

    Completed tasks 1 and 2

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,6 +1,6 @@
 function calculateAverageSales(Sales) {
 return Sales.length===0? 0: Sales.reduce((sum,Sale)=> sum+ Sale,0)/Sales.length;
-}// The program will return 0 if no information is entered. Otherwise
+}// The program will return 0 if no information is entered. Otherwise, the values in the array will be added and then divided by the numnber of values to determine the average
 let SalesFigures= [4590,18400,2130,8800]
 let average = calculateAverageSales (SalesFigures);
 console.log ('The average sales are', average);
@@ -16,6 +16,25 @@ if (averageSales>10000){
     } else {
         return "Needs Improvement";
     }
-}
-let perfating= determinePerformanceRating (averageSales);
+}// this section of the code is a simple series of if-else that displays different messages based on what the average value is.
+let performanceRating = determinePerformanceRating(SalesFigures);
 
+console.log('Average Sales Rating is', performanceRating)
+
+const salesteam= [
+    {name: 'Alex',TotalSales: 17000},
+    {name: 'Shia',TotalSales: 9000},
+    {name: 'Burt',TotalSales: 5000},
+    {name: 'Shia',TotalSales: 1000},
+];
+function findTopandBottomPerformers (salesteam){
+    let MaxSales= Math.max (salesteam.map (name=>name.TotalSales));
+    let MinSales= Math.min (salesteam.map (name=> name.TotalSales));
+    return MaxSales
+    
+    
+}
+console.log ('The top performer is:', );
+console.log ('The bottom performer is:', );
+
+let topperformer=findTopandBottomPerformers (salesteam);
